6721f08650984eb37a38e05697f816ee4c37f1b7
Soultane <152623944+raqisoultane@users.noreply.github.com>
2024-10-29 20:44:39 +0100

    Delete Python Projects/project.py

--- Python Projects/project.py
+++
@@ -1,9 +0,0 @@
-name = input("Ach smitak a zEEENNN? ")
-print("Wachh, " + name + " Mar7ba bik zEEENNN !!")
-#phyton 
-should_we_play = input("Nla3bo al GHZALLA? ")
-
-if should_we_play == "AH":
-  print("OK 7biba")
-else:
-  print("Nchadak nma7nak")
